fe65232195830b3194b4fb8cba3d950b9788b279
waylandbn <morrisbranson@gmail.com>
2025-08-07 18:58:00 -0500

    front title.tsx

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -45,8 +45,7 @@ export const HeroSection = ({
             initial={{ opacity: 0, y: 20 }}
             transition={{ delay: 0.2, duration: 0.8, ease: [0.16, 1, 0.3, 1] }}
           >
-            Hi, I&apos;m {name} &mdash; I design &amp; code modern web
-            experiences.
+            Howdy, I&apos;m {name}
           </motion.h1>
 
           <motion.p
