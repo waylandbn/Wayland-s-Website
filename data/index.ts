565054e0b8eacfd29aaf98e01378381bda34e2fe
Wayland Branson <morrisbranson@gmail.com>
2025-09-28 18:19:27 -0500

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -225,17 +225,17 @@ export const DATA = {
         id: 3,
         title: "88.1 KNTU",
         description:
-          "Rebranded 88.1 KNTU with a modern visual identity and unified brand voice, updating its presence across social media, website, and on-air promotions.",
+          "Station Programmer at KNTU I organized promotions, led digital strategy, helped build a new website, interviewed artists, programmed the weekly playlist, and grew weekly listeners.",
         image: "https://www.dropbox.com/scl/fi/jcj3kbjetwbc5gz5nb4zo/kntu-4.png?rlkey=6yh6sadptx2br7sxntpp8upog&st=db4uh5ep&raw=1",
         gallery: [
-          "https://www.dropbox.com/scl/fi/zhg7y55buao4319qre3y1/2-kntu.png?rlkey=rqwfjw3s1u28fhys0isp25lj8&st=s1w7ml2j&raw=1",
+          "https://www.dropbox.com/scl/fi/ok4a3v3vg4qt95nuva6i1/KNTU-Listners-Wayland-Branson.png?rlkey=s9dqrpnmjd3lbuk3uggwe2zgb&raw=1",
           "https://www.dropbox.com/scl/fi/yckqv9p4gax5u9iuxg0bs/kntu-5.png?rlkey=v1q6l1adb2vk5trwyr6kyiq7e&st=my9u4cji&raw=1",
           "https://www.dropbox.com/scl/fi/rdanuvkoh28e40l97q28m/3.-kntu.png?rlkey=syh782s5p9kqj8h8x3fn31jd4&st=n3episjy&raw=1",
           "https://www.dropbox.com/scl/fi/z4j42m50wdmuvsdsdrwbh/1-KNTU.png?rlkey=bhvmizywh9bn4bg6mpgjut5k3&st=91gy9r5y&raw=1",
         ],
         category: "Campaigns",
         details:
-          "Rebranded 88.1 KNTU to modernize its image and reflect evolving music programming. Delivered a refreshed visual identity, established a consistent brand voice, and expanded the station’s digital presence across social media, website, and on-air promotions, resulting in stronger audience engagement and recognition.",
+          "As Station Programmer at KNTU I planned and ran promotional events, managed digital channels and content, collaborated to design and launch a new website, booked and interviewed artists, designed and scheduled the weekly playlist, acted as the point person for underwriting requests, record labels, artists, and promoters, tracked results and refined programming and posts to lift weekly listeners.",
         github: "https://kntu.com",
         tech: [
           { name: "Adobe Photoshop", icon: "devicon:photoshop" },
